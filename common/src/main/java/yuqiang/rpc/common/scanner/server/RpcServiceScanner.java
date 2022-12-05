2c16cb859fcab4689a8d1e5678c0bcfedfa7d6df
yuqiang <yuqiang@zhixueyun.com>
2022-12-05 08:55:01 +0800

    服务提供者和收发消息的基础功能实现

--- common/src/main/java/yuqiang/rpc/common/scanner/server/RpcServiceScanner.java
+++ common/src/main/java/yuqiang/rpc/common/scanner/server/RpcServiceScanner.java
@@ -12,17 +12,17 @@ import java.util.Map;
 public class RpcServiceScanner extends ClassScanner {
     private static final Logger LOGGER = LoggerFactory.getLogger(RpcServiceScanner.class);
 
-    public static Map<String, Object> doScannerWithRpcServiceAnnotationFilterAndRegistryService(/*String host, int port, */ String scanPackage/*, RegistryService registryService*/) throws Exception{
+    public static Map<String, Object> doScannerWithRpcServiceAnnotationFilterAndRegistryService(/*String host, int port, */ String scanPackage/*, RegistryService registryService*/) throws Exception {
         Map<String, Object> handlerMap = new HashMap<>();
         List<String> classNameList = getClassNameList(scanPackage);
-        if (classNameList == null || classNameList.isEmpty()){
+        if (classNameList == null || classNameList.isEmpty()) {
             return handlerMap;
         }
         classNameList.stream().forEach((className) -> {
             try {
                 Class<?> clazz = Class.forName(className);
                 RpcService rpcService = clazz.getAnnotation(RpcService.class);
-                if (rpcService != null){
+                if (rpcService != null) {
                     //优先使用interfaceClass, interfaceClass的name为空，再使用interfaceClassName
                     //TODO 后续逻辑向注册中心注册服务元数据，同时向handlerMap中记录标注了RpcService注解的类实例
                     LOGGER.info("当前标注了@RpcService注解的类实例名称===>>> " + clazz.getName());
@@ -31,6 +31,9 @@ public class RpcServiceScanner extends ClassScanner {
                     LOGGER.info("interfaceClassName===>>> " + rpcService.interfaceClassName());
                     LOGGER.info("version===>>> " + rpcService.version());
                     LOGGER.info("group===>>> " + rpcService.group());
+                    String serviceName = getServiceName(rpcService);
+                    String key = serviceName.concat(rpcService.version()).concat(rpcService.group());
+                    handlerMap.put(key, clazz.newInstance());
                 }
             } catch (Exception e) {
                 LOGGER.error("scan classes throws exception: {}", e);
@@ -42,17 +45,17 @@ public class RpcServiceScanner extends ClassScanner {
     /**
      * 获取serviceName
      */
-    private static String getServiceName(RpcService rpcService){
+    private static String getServiceName(RpcService rpcService) {
         //优先使用interfaceClass
         Class clazz = rpcService.interfaceClass();
-        if (clazz == void.class){
+        if (clazz == void.class) {
             return rpcService.interfaceClassName();
         }
         String serviceName = clazz.getName();
-        if (serviceName == null || serviceName.trim().isEmpty()){
+        if (serviceName == null || serviceName.trim().isEmpty()) {
             serviceName = rpcService.interfaceClassName();
         }
         return serviceName;
     }
 
- }
+}
---
+++ provider/provider-native/src/main/java/yuqiang/rpc/provider/RpcSingleServer.java
@@ -0,0 +1,23 @@
+package yuqiang.rpc.provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import yuqiang.rpc.common.scanner.server.RpcServiceScanner;
+import yuqiang.rpc.provider.common.server.base.BaseServer;
+
+/**
+ * 以java原生的应用启动java服务
+ */
+public class RpcSingleServer extends BaseServer {
+    private final Logger logger = LoggerFactory.getLogger(RpcSingleServer.class);
+
+    public RpcSingleServer(String serverAddress, String scanPackage) {
+        //调用父类构造方法
+        super(serverAddress);
+        try {
+            this.handlerMap = RpcServiceScanner.doScannerWithRpcServiceAnnotationFilterAndRegistryService(scanPackage);
+        } catch (Exception e) {
+            logger.error("RPC Server init error", e);
+        }
+    }
+}
