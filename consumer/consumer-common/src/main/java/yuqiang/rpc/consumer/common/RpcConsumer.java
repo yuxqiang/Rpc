85406373e3b423e3f70a5f6b6dfe7781da279cd4
yuqiang <yuqiang@zhixueyun.com>
2022-12-06 09:30:55 +0800

    添加rpc异步转同步的实际方法实现

--- consumer/consumer-common/src/main/java/yuqiang/rpc/consumer/common/RpcConsumer.java
+++ consumer/consumer-common/src/main/java/yuqiang/rpc/consumer/common/RpcConsumer.java
@@ -1,7 +1,6 @@
 package yuqiang.rpc.consumer.common;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.EventLoopGroup;
@@ -14,10 +13,8 @@ import yuqiang.rpc.consumer.common.initialzer.RpcConsumerInitializer;
 import yuqiang.rpc.potocol.RpcProtocol;
 import yuqiang.rpc.potocol.request.RpcRequest;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class RpcConsumer {
 
@@ -53,25 +50,25 @@ public class RpcConsumer {
         eventLoopGroup.shutdownGracefully();
     }
 
-    public void sendRequest(RpcProtocol<RpcRequest> protocol) throws Exception {
+    public Object sendRequest(RpcProtocol<RpcRequest> protocol) throws Exception {
         String serviceAddress = "127.0.0.1";
         int port = 21880;
         String key = serviceAddress.concat("_").concat(String.valueOf(port));
         RpcConsumerHandler rpcConsumerHandler = handlerMap.get(key);
-        if(rpcConsumerHandler==null){
-            rpcConsumerHandler=getRpcConsumerHandler(serviceAddress,port);
-            handlerMap.put(key,rpcConsumerHandler);
-        }else if(!rpcConsumerHandler.getChannel().isActive()){
+        if (rpcConsumerHandler == null) {
+            rpcConsumerHandler = getRpcConsumerHandler(serviceAddress, port);
+            handlerMap.put(key, rpcConsumerHandler);
+        } else if (!rpcConsumerHandler.getChannel().isActive()) {
             rpcConsumerHandler.close();
-            rpcConsumerHandler=getRpcConsumerHandler(serviceAddress,port);
-            handlerMap.put(key,rpcConsumerHandler);
+            rpcConsumerHandler = getRpcConsumerHandler(serviceAddress, port);
+            handlerMap.put(key, rpcConsumerHandler);
         }
-        rpcConsumerHandler.sendRequest(protocol);
+        return rpcConsumerHandler.sendRequest(protocol);
     }
 
     private RpcConsumerHandler getRpcConsumerHandler(String serviceAddress, int port) throws InterruptedException {
-        ChannelFuture channelFuture= bootstrap.connect(serviceAddress,port).sync();
-        channelFuture.addListener((ChannelFutureListener) listener->{
+        ChannelFuture channelFuture = bootstrap.connect(serviceAddress, port).sync();
+        channelFuture.addListener((ChannelFutureListener) listener -> {
             if (channelFuture.isSuccess()) {
                 logger.info("connect rpc server {} on port {} success.", serviceAddress, port);
             } else {
--- consumer/consumer-common/src/main/java/yuqiang/rpc/consumer/common/handler/RpcConsumerHandler.java
+++ consumer/consumer-common/src/main/java/yuqiang/rpc/consumer/common/handler/RpcConsumerHandler.java
@@ -9,15 +9,20 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import yuqiang.rpc.potocol.RpcProtocol;
+import yuqiang.rpc.potocol.header.RpcHeader;
 import yuqiang.rpc.potocol.request.RpcRequest;
 import yuqiang.rpc.potocol.response.RpcResponse;
 
 import java.net.SocketAddress;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class RpcConsumerHandler extends SimpleChannelInboundHandler<RpcProtocol<RpcResponse>> {
     private final Logger logger = LoggerFactory.getLogger(RpcConsumerHandler.class);
     private volatile Channel channel;
     private SocketAddress remotePeer;
+
+    private Map<Long, RpcProtocol<RpcResponse>> pendingResponse = new ConcurrentHashMap<>();
 
     public Channel getChannel() {
         return channel;
@@ -40,6 +45,7 @@ public class RpcConsumerHandler extends SimpleChannelInboundHandler<RpcProtocol<
         super.channelActive(ctx);
         this.remotePeer = this.channel.remoteAddress();
     }
+
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
         super.channelRegistered(ctx);
@@ -48,18 +54,29 @@ public class RpcConsumerHandler extends SimpleChannelInboundHandler<RpcProtocol<
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, RpcProtocol<RpcResponse> protocol) throws Exception {
-        if (protocol == null){
+        if (protocol == null) {
             return;
         }
         logger.info("服务消费者接收到的数据===>>>{}", JSONObject.toJSONString(protocol));
+        RpcHeader rpcHeader = protocol.getHeader();
+        long requestId = rpcHeader.getRequestId();
+        pendingResponse.put(requestId,protocol);
     }
 
     /**
      * 服务消费者向服务提供者发送请求
      */
-    public void sendRequest(RpcProtocol<RpcRequest> protocol){
+    public Object sendRequest(RpcProtocol<RpcRequest> protocol) {
         logger.info("服务消费者发送的数据===>>>{}", JSONObject.toJSONString(protocol));
         channel.writeAndFlush(protocol);
+        RpcHeader rpcHeader = protocol.getHeader();
+        long requestId = rpcHeader.getRequestId();
+        while (true) {
+            RpcProtocol<RpcResponse> responseRpcProtocol = pendingResponse.remove(requestId);
+            if (responseRpcProtocol != null) {
+                return responseRpcProtocol.getBody().getResult();
+            }
+        }
     }
 
     public void close() {
